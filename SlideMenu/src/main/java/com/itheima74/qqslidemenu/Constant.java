3ce841fdff425032bd7d24207df2bac86218bb0e
lihuichao <664871385@qq.com>
2016-08-26 11:23:11 +0800

    Initial commit

---
+++ SlideMenu/src/main/java/com/itheima74/qqslidemenu/Constant.java
@@ -0,0 +1,20 @@
+package com.itheima74.qqslidemenu;
+
+public interface Constant {
+
+	public static final String[] sCheeseStrings = {
+			"SlideMenu_1", "SlideMenu_2", "SlideMenu_3", "SlideMenu_4", "SlideMenu_5",
+			"SlideMenu_6", "SlideMenu_7", "SlideMenu_8", "SlideMenu_9", "SlideMenu_10",
+			"SlideMenu_11", "SlideMenu_12", "SlideMenu_13", "SlideMenu_14", "SlideMenu_15",
+			"SlideMenu_16", "SlideMenu_17", "SlideMenu_18", "SlideMenu_19", "SlideMenu_20",
+			"SlideMenu_21", "SlideMenu_22", "SlideMenu_23", "SlideMenu_24", "SlideMenu_25",
+	};
+
+	public static final String[] NAMES = new String[]{
+			"MainView_1", "MainView_2", "MainView_3", "MainView_4", "MainView_5",
+			"MainView_6", "MainView_7", "MainView_8", "MainView_9", "MainView_10",
+			"MainView_11", "MainView_12", "MainView_13", "MainView_14", "MainView_15",
+			"MainView_16", "MainView_17", "MainView_18", "MainView_19", "MainView_20",
+			"MainView_21", "MainView_22", "MainView_23", "MainView_24", "MainView_25",
+	};
+}
